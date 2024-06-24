3ea4742c290176012e275ea5d360fc5203647793
synakhanna <sk9921@nyu.edu>
2024-06-24 12:09:08 -0700

    Adding Initial Files

---
+++ forthix-website/my-react-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+
+function App() {
+
+  return (
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">
+            Hello, Bulma!
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ forthix-website/my-react-app/src/navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return <nav className="nav">
+    <a href="/" className="Forthix">Forthix</a>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+        <a href="/read">Read</a>
+        <a href="/learn">Learn</a>
+        <a href="/faq">FAQ</a>
+        <a href="/about">About</a>
+      </li>
+    </ul>
+  </nav>
+}

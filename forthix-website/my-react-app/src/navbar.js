81ac42e49e8d6a35a2e2405fcc3a84cb924395da
synakhanna <sk9921@nyu.edu>
2024-07-15 11:30:57 -0700

    wip (implemented initial homepage)

--- forthix-website/my-react-app/src/navbar.js
+++ forthix-website/my-react-app/src/navbar.js
@@ -1,14 +1,16 @@
+import './Navbar.css';
+import logo from './logo.png';
+
 export default function Navbar() {
-  return <nav className="nav">
-    <a href="/" className="Forthix">Forthix</a>
-    <ul>
-      <li>
-        <a href="/">Home</a>
-        <a href="/read">Read</a>
-        <a href="/learn">Learn</a>
-        <a href="/faq">FAQ</a>
-        <a href="/about">About</a>
-      </li>
-    </ul>
-  </nav>
+  return (
+    <nav className="nav">
+      <div className="menu">
+  				<a href="/"><img src={logo} alt="Forthix logo" className="logo"/></a>
+  				<a href="/" class="menuoptions">Home</a>
+					<a href="/learn" class="menuoptions">Learn</a>
+					<a href="/faq" class="menuoptions">FAQ</a>
+          <a href="https://marketplace.atlassian.com/apps/1234620/orgjql?hosting=cloud&tab=overview" target="_blank" className="get-forthix-button">Get Forthix</a>
+				</div>
+    </nav>
+  );
 }
